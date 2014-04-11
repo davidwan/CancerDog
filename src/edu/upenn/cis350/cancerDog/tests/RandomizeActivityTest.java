b1e8735c7128fc5dc9a8b62b51dcc6190aa1a699
David <defuwan@seas.upenn.edu>
2014-04-11 13:18:01 -0400

    testing framework

---
+++ src/edu/upenn/cis350/cancerDog/tests/RandomizeActivityTest.java
@@ -0,0 +1,10 @@
+package edu.upenn.cis350.cancerDog.tests;
+
+import edu.upenn.cis350.cancerDog.RandomizeActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class RandomizeActivityTest extends ActivityInstrumentationTestCase2<RandomizeActivity>{
+	public RandomizeActivityTest () {
+        super(RandomizeActivity.class);
+    } 
+}
